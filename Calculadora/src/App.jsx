153ff5cc8f7887471fa1ab9d41c1369f92711cdc
Fillipe Saque <fillipesaque@gmail.com>
2024-09-19 11:31:18 -0300

    Calculadora

---
+++ Calculadora/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Calculator from './components/calculator.jsx'
+function App() {
+  
+
+  return (
+    <>
+      <div>
+          <h2>Calculadora</h2>
+          <Calculator/>
+      </div>
+    </>
+  )
+}
+
+export default App
